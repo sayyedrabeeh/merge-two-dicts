4d32d97e710e3d536bff6fed530a67034129669d
sayyedrabeeh <sayyedrabeeh240@gmail.com>
2024-08-16 09:59:47 +0530

    Add files via upload

---
+++ mergr two dict.py
@@ -0,0 +1,18 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+dict1 = {'a': 10, 'b': 20}
+dict2 = {'b': 30, 'c': 40}
+di={**dict1,**dict2}
+
+# for i, in dict1:
+#     di[i]=dict1[i]
+# for i in dict2:
+#     di[i]=dict2[i]
+    
+print(di)
